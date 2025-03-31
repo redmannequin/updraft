913b334de7d90b841402f64a8a36bc75815436a2
Flavio B. <flavio.b.github@gmail.com>
2025-03-31 23:12:40 +0100

    fix config

--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -8,7 +8,11 @@ async fn main() -> anyhow::Result<()> {
     dotenv().context("failed to load .env")?;
 
     let config: AppConfig = Config::builder()
-        .add_source(Environment::with_prefix("APP"))
+        .add_source(
+            Environment::with_prefix("APP")
+                .prefix_separator("_")
+                .separator("__"),
+        )
         .build()
         .context("config build")?
         .try_deserialize()
